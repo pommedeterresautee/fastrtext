181103c1ae60169af64d1e7efdd2295434516fb6
pommedeterresautee <pommedeterresautee@msn.com>
2017-08-07 22:59:50 +0200

    add main call

--- src/fastRtext.cpp
+++ src/fastRtext.cpp
@@ -1,8 +1,9 @@
 // [[Rcpp::plugins(cpp11)]]
 
 #include <Rcpp.h>
-// to compile with sourceCpp, need to change the path of the headers
 #include "fasttext.h"
+#include "main.h"
+
 using namespace Rcpp;
 using namespace fasttext;
 
@@ -23,6 +24,29 @@ public:
     std::string stringPath(path(0));
     this->model->loadModel(stringPath);
     this->model_loaded = true;
+  }
+
+  void execute(CharacterVector commands) {
+    int num_argc = commands.size();
+
+    if (num_argc < 2) {
+      stop("Not enough parameters");
+      return;
+    }
+
+    char** cstrings = new char*[commands.size()];
+    for(size_t i = 0; i < commands.size(); ++i) {
+      std::string command(commands[i]);
+      cstrings[i] = new char[command.size() + 1];
+      std::strcpy(cstrings[i], command.c_str());
+    }
+
+    main(num_argc, cstrings);
+
+    for(size_t i = 0; i < num_argc; ++i) {
+      delete[] cstrings[i];
+    }
+    delete[] cstrings;
   }
 
   List predict(CharacterVector documents, int k = 1) {
@@ -65,11 +89,13 @@ RCPP_MODULE(FastRtext) {
   class_<FastRtext>("FastRtext")
   .constructor()
   .method("load", &FastRtext::load, "Load a model")
-  .method("predict", &FastRtext::predict, "Make a prediction");
+  .method("predict", &FastRtext::predict, "Make a prediction")
+  .method("execute", &FastRtext::execute, "Execute commands");
 }
 
 /*** R
 model <- new(FastRtext)
+model$execute(c("f", "supervised"))
 model$load("/home/geantvert/model.bin") # requires to have a model there
 model$predict(c("this is a test", "another test"), 3)
 rm(model)
---
+++ src/main.h
@@ -0,0 +1,7 @@
+#ifndef FASTTEXT_MAIN_H
+#define FASTTEXT_MAIN_H
+
+void analogies(int argc, char** argv);
+void train(int argc, char** argv);
+int main(int argc, char** argv);
+#endif //FASTTEXT_MAIN_H
