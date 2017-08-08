4e0784e42fd9ba9c161c2025cd20952b903ad482
pommedeterresautee <pommedeterresautee@msn.com>
2017-08-08 23:46:27 +0200

    merge jfasttext

--- src/fastRtext.cpp
+++ src/fastRtext.cpp
@@ -1,16 +1,17 @@
 // [[Rcpp::plugins(cpp11)]]
 
 #include <Rcpp.h>
-#include "fasttext.h"
-#include "main.h"
+#include "fasttext_wrapper.h"
+//#include "main.h"
 
 using namespace Rcpp;
-using namespace fasttext;
+using namespace FastTextWrapper;
 
 class FastRtext{
 public:
   ~FastRtext(){
-    this->model.reset();
+    model->unloadModel();
+    model.reset();
   }
 
   void load(CharacterVector path) {
@@ -20,10 +21,10 @@ public:
     if(!std::ifstream(path[0])){
       stop("Path doesn't point to a file: " + path[0]);
     }
-    this->model.reset(new FastText);
+    model.reset(new FastTextApi);
     std::string stringPath(path(0));
-    this->model->loadModel(stringPath);
-    this->model_loaded = true;
+    model->loadModel(stringPath);
+    model_loaded = true;
   }
 
   //' @param commands commands to execute
@@ -42,7 +43,7 @@ public:
       std::strcpy(cstrings[i], command.c_str());
     }
 
-    main(num_argc, cstrings);
+    model->runCmd(num_argc, cstrings);
 
     for(size_t i = 0; i < num_argc; ++i) {
       delete[] cstrings[i];
@@ -56,27 +57,26 @@ public:
 
     for(int i = 0; i < documents.size(); ++i){
       std::string s(documents(i));
-      std::istringstream istr (s);
-      std::vector<std::pair<real, std::string> > predictions;
-      model->predict(istr, k, predictions);
+      std::vector<std::pair<real, std::string> > predictions = model->predictProba(s, k);
       NumericVector logProbabilities(predictions.size());
       CharacterVector labels(predictions.size());
-
       for (int j = 0; j < predictions.size() ; ++j){
         logProbabilities[j] = predictions[j].first;
         labels[j] = predictions[j].second;
       }
-
       NumericVector probabilities(exp(logProbabilities));
       probabilities.attr("names") = labels;
       list[i] = probabilities;
     }
-
     return list;
   }
 
+  NumericVector printVector(std::string word){
+    return wrap(model->getVector(word));
+  }
+
 private:
-  std::shared_ptr<FastText> model;
+  std::shared_ptr<FastTextApi> model;
   bool model_loaded = false;
 
   void check_model(){
@@ -91,7 +91,9 @@ RCPP_MODULE(FastRtext) {
   .constructor()
   .method("load", &FastRtext::load, "Load a model")
   .method("predict", &FastRtext::predict, "Make a prediction")
-  .method("execute", &FastRtext::execute, "Execute commands");
+  .method("execute", &FastRtext::execute, "Execute commands")
+  .method("printVector", &FastRtext::printVector, "Get word vector");
+
 }
 
 /*** R
@@ -99,5 +101,6 @@ model <- new(FastRtext)
 model$execute(c("f", "supervised"))
 model$load("/home/geantvert/model.bin") # requires to have a model there
 model$predict(c("this is a test", "another test"), 3)
+model$printVector("département")
 rm(model)
 */
---
+++ src/fasttext_wrapper.h
@@ -0,0 +1,58 @@
+#ifndef FASTTEXT_WRAPPER_H
+#define FASTTEXT_WRAPPER_H
+
+#include "fasttext.h"
+#include "fasttext_wrapper_misc.h"
+
+/**
+* FastText's wrapper
+*/
+namespace FastTextWrapper {
+
+using namespace fasttext;
+
+class FastTextApi {
+private:
+  FastText fastText;
+  FastTextPrivateMembers* privateMembers;
+public:
+  FastTextApi();
+  // We don't make runCmd() a static method so that Loader.load() is always be called in FastTextApi().
+  void runCmd(int, char **);
+  bool checkModel(const std::string&);
+  void loadModel(const std::string&);
+  // Model unloading is not available in fastText C++. This method is added since
+  // Java's GC doesn't collect memory allocated by native function calls.
+  void unloadModel();
+  void test(const std::string&, int32_t);
+  // TODO: Check if model was loaded
+  std::vector<std::string> predict(const std::string&, int32_t);
+  std::vector<std::pair<real,std::string>> predictProba(const std::string&, int32_t);
+  std::vector<real> getVector(const std::string&);
+  std::vector<std::string> getWords();
+  std::vector<std::string> getLabels();
+  std::string getWord(int32_t);
+  int32_t getNWords();
+  std::string getLabel(int32_t);
+  int32_t getNLabels();
+  double getLr();
+  int getLrUpdateRate();
+  int getDim();
+  int getContextWindowSize();
+  int getEpoch();
+  int getMinCount();
+  int getMinCountLabel();
+  int getNSampledNegatives();
+  int getWordNgrams();
+  std::string getLossName();
+  std::string getModelName();
+  int getBucket();
+  int getMinn();
+  int getMaxn();
+  double getSamplingThreshold();
+  std::string getLabelPrefix();
+  std::string getPretrainedVectorsFileName();
+};
+}
+
+#endif
