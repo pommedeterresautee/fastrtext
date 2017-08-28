13375d8a5435ff59652b9e254775249700e7b89d
pommedeterresautee <pommedeterresautee@msn.com>
2017-08-28 22:32:45 +0200

    test

--- tests/testthat/test-training.R
+++ tests/testthat/test-training.R
@@ -37,26 +37,26 @@ test_that("Unsupervised training", {
 #   gc()
 })
 #
-# test_that("Supervised training", {
-#   data("train_sentences")
-#   data("test_sentences")
-#
-#   # prepare data
-#   tmp_file_model <- tempfile()
-#
-#   train_labels <- paste0("__label__", train_sentences[,"class.text"])
-#   train_texts <- tolower(train_sentences[,"text"])
-#   train_to_write <- paste(train_labels, train_texts)
-#   train_tmp_file_txt <- tempfile()
-#   writeLines(text = train_to_write, con = train_tmp_file_txt)
-#
-#   test_labels <- paste0("__label__", test_sentences[,"class.text"])
-#   test_texts <- tolower(test_sentences[,"text"])
-#   test_to_write <- paste(test_labels, test_texts)
-#
-#   # learn model
-#   execute(commands = c("supervised", "-input", train_tmp_file_txt, "-output", tmp_file_model, "-dim", 20, "-lr", 1, "-epoch", 20, "-wordNgrams", 2, "-verbose", 1))
-#
+test_that("Supervised training", {
+  data("train_sentences")
+  data("test_sentences")
+
+  # prepare data
+ tmp_file_model <- tempfile()
+
+  train_labels <- paste0("__label__", train_sentences[,"class.text"])
+  train_texts <- tolower(train_sentences[,"text"])
+  train_to_write <- paste(train_labels, train_texts)
+  train_tmp_file_txt <- tempfile()
+  writeLines(text = train_to_write, con = train_tmp_file_txt)
+
+  test_labels <- paste0("__label__", test_sentences[,"class.text"])
+  test_texts <- tolower(test_sentences[,"text"])
+  test_to_write <- paste(test_labels, test_texts)
+
+  # learn model
+  execute(commands = c("supervised", "-input", train_tmp_file_txt, "-output", tmp_file_model, "-dim", 20, "-lr", 1, "-epoch", 20, "-wordNgrams", 2, "-verbose", 0))
+
 #   # load model
 #   model <- load_model(tmp_file_model)
 #   predictions <- predict(model, sentences = test_to_write)
@@ -82,4 +82,4 @@ test_that("Unsupervised training", {
 #   unlink(tmp_file_model)
 #   rm(model)
 #   gc()
-# })
+})
