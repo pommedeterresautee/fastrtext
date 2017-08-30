e937c397670d38a3b0ef0af2a8953b3589faef09
pommedeterresautee <pommedeterresautee@msn.com>
2017-08-30 20:46:58 +0200

    Remove warnings

--- R/zzz.R
+++ R/zzz.R
@@ -9,3 +9,22 @@ loadModule("FastRText", TRUE)
 #' @importFrom Rcpp evalCpp loadModule cpp_object_initializer
 #' @exportPattern "^[[:alpha:]]+"
 NULL
+
+#' Rcpp_FastRText class.
+#'
+#' Some details about this class and my plans for it in the body.
+#'
+#' @slot load Load a model
+#' @slot predict Make a prediction
+#' @slot execute Execute commands
+#' @slot get_vectors Get vectors related to provided words
+#' @slot get_parameters Get parameters used to train the model
+#' @slot get_words List all words learned
+#' @slot get_labels List all labels learned
+#' @name Rcpp_FastRText-class
+
+NULL
+
+#' C++ object
+#' @name C++Object-class
+NULL
