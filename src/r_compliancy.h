40b119c4f9355222d08013c1f44819795646101d
pommedeterresautee <pommedeterresautee@msn.com>
2017-08-30 09:36:59 +0200

    refactoring

--- src/r_compliancy.h
+++ src/r_compliancy.h
@@ -6,7 +6,7 @@
 #include <Rcpp.h>
 #include <stdexcept>
 
-#define exit(param) Rcpp::stop("Exit code: " + std::to_string(param));
+#define exit(param) throw std::runtime_error("Exit code: " + std::to_string(param));
 #define cerr cout
 
 #endif //R_COMPLIANCY
