a8c9dce140a3c402b01ef5844a20cc60beeefdf2
pommedeterresautee <pommedeterresautee@msn.com>
2017-11-06 19:54:49 +0100

    update website

--- R/API.R
+++ R/API.R
@@ -132,10 +132,6 @@ get_word_vectors <- function(model, words = get_dictionary(model)) {
 #'
 #' Use the same commands than the one to use for the command line.
 #'
-#' WARNING: for process using multi-threading,
-#' displaying information should be disabled (`-verbose 0`).
-#' This is due to incompatibility between `Rcpp::Rcout` and `pthread`.
-#'
 #' @param commands [character] of commands
 #' @examples
 #' \dontrun{
